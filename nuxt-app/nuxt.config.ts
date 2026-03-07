7298e1c767d88cdd2a3d7ecfe618537acbbf3012
Alexandr <Tishechkin1994@gmail.com>
2026-03-07 18:45:04 +0300

    1

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/main.css'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
   runtimeConfig: {
     public: {
       tracknodeApiKey: process.env.NUXT_PUBLIC_TRACKNODE_API_KEY || '',
